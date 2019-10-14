b6ad69a8ef60ec23ccdb5bc88e1a5f2f882b870f
Mohamed Meabed <mo.meabed@gmail.com>
2019-10-14 15:07:05 +0400

    add templates, generate files with correct regions

--- variables.tf
+++ variables.tf
@@ -44,8 +44,6 @@ variable "target_regions" {
     "ap-southeast-2",
     "ap-northeast-1",
     "ca-central-1",
-    "cn-north-1",
-    "cn-northwest-1",
     "eu-central-1",
     "eu-west-1",
     "eu-west-2",
