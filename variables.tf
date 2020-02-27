269731560e6f33609d992f6ddb27253116bdc5a3
Faishal Saiyed <saiyedfaishal@gmail.com>
2020-02-27 15:21:34 -0500

    Removed vpc baselines, output and variables.

--- variables.tf
+++ variables.tf
@@ -163,29 +163,6 @@ variable "allow_users_to_change_password" {
 }
 
 # --------------------------------------------------------------------------------------------------
-# Variables for vpc-baseline module.
-# --------------------------------------------------------------------------------------------------
-variable "vpc_iam_role_name" {
-  description = "The name of the IAM Role which VPC Flow Logs will use."
-  default     = "VPC-Flow-Logs-Publisher"
-}
-
-variable "vpc_iam_role_policy_name" {
-  description = "The name of the IAM Role Policy which VPC Flow Logs will use."
-  default     = "VPC-Flow-Logs-Publish-Policy"
-}
-
-variable "vpc_log_group_name" {
-  description = "The name of CloudWatch Logs group to which VPC Flow Logs are delivered."
-  default     = "default-vpc-flow-logs"
-}
-
-variable "vpc_log_retention_in_days" {
-  description = "Number of days to retain logs for. CIS recommends 365 days.  Possible values are: 0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653. Set to 0 to keep logs indefinitely."
-  default     = 365
-}
-
-# --------------------------------------------------------------------------------------------------
 # Variables for config-baseline module.
 # --------------------------------------------------------------------------------------------------
 
