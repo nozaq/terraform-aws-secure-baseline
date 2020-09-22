0ffae4cba464b60fb5c30511ebb84bf071188650
nozaq <code@nozaq.com>
2020-09-22 17:03:37 +0900

    feat: add an organizations monitoring alarm

--- compliance.md
+++ compliance.md
@@ -69,7 +69,7 @@ Implementation status for each item is categorized as follows.
 | 4.12 | Ensure a log metric filter and alarm exist for changes to network gateways | OK | |
 | 4.13 | Ensure a log metric filter and alarm exist for route table changes | OK | |
 | 4.14 | Ensure a log metric filter and alarm exist for VPC changes | OK | |
----| 4.15 | Ensure a log metric filter and alarm exist for AWS Organizations changes | N/A | |
+| 4.15 | Ensure a log metric filter and alarm exist for AWS Organizations changes | N/A | |
 | 5.1 | Ensure no Network ACLs allow ingress from 0.0.0.0/0 to remote server administration ports | PARTIAL | All default Network ACLs are configured to have no ingress rules. |
 | 5.2 | Ensure no security groups allow ingress from 0.0.0.0/0 to remote server administration ports | PARTIAL | All default security groups are configured to have no ingress rules. |
 | 5.3  | Ensure the default security group of every VPC restricts all traffic | OK | |
--- modules/alarm-baseline/main.tf
+++ modules/alarm-baseline/main.tf
@@ -475,3 +475,36 @@ resource "aws_cloudwatch_metric_alarm" "vpc_changes" {
 
   tags = var.tags
 }
+
+resource "aws_cloudwatch_log_metric_filter" "organizations_changes" {
+  count = var.enabled ? 1 : 0
+
+  name           = "OrganizationsChanges"
+  pattern        = "{ ($.eventSource = organizations.amazonaws.com) && (($.eventName = \"AcceptHandshake\") || ($.eventName = \"AttachPolicy\") || ($.eventName = \"CreateAccount\") || ($.eventName = \"CreateOrganizationalUnit\") || ($.eventName= \"CreatePolicy\") || ($.eventName = \"DeclineHandshake\") || ($.eventName = \"DeleteOrganization\") || ($.eventName = \"DeleteOrganizationalUnit\") || ($.eventName = \"DeletePolicy\") || ($.eventName = \"DetachPolicy\") || ($.eventName = \"DisablePolicyType\") || ($.eventName = \"EnablePolicyType\") || ($.eventName = \"InviteAccountToOrganization\") || ($.eventName = \"LeaveOrganization\") || ($.eventName = \"MoveAccount\") || ($.eventName = \"RemoveAccountFromOrganization\") || ($.eventName = \"UpdatePolicy\") || ($.eventName =\"UpdateOrganizationalUnit\")) }"
+  log_group_name = var.cloudtrail_log_group_name
+
+  metric_transformation {
+    name      = "OrganizationsChanges"
+    namespace = var.alarm_namespace
+    value     = "1"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "organizations_changes" {
+  count = var.enabled ? 1 : 0
+
+  alarm_name                = "OrganizationsChanges"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "1"
+  metric_name               = aws_cloudwatch_log_metric_filter.organizations_changes[0].id
+  namespace                 = var.alarm_namespace
+  period                    = "300"
+  statistic                 = "Sum"
+  threshold                 = "1"
+  alarm_description         = "Monitoring AWS Organizations changes can help you prevent any unwanted, accidental or intentional modifications that may lead to unauthorized access or other security breaches."
+  alarm_actions             = [aws_sns_topic.alarms[0].arn]
+  treat_missing_data        = "notBreaching"
+  insufficient_data_actions = []
+
+  tags = var.tags
+}
