a28e9a39a3d707cda23e8ce3bd249096cf61406d
Steph Gosling <steph@chuci.org>
2020-11-19 14:40:59 +0000

    fix: specify type for the tag variable as this is broken in terraform 0.13.4

--- modules/secure-bucket/variables.tf
+++ modules/secure-bucket/variables.tf
@@ -21,7 +21,8 @@ variable "enabled" {
 
 variable "tags" {
   description = "Specifies object tags key and value. This applies to all resources created by this module."
-  default = {
+  type        = map(string)
+  default     = {
     "Terraform" = true
   }
 }
