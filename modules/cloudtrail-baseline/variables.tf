13ac5599fc4cea668bc3f877bbc477ed1dadc950
Steph Gosling <steph@chuci.org>
2020-11-19 16:04:54 +0000

    fix: add type to tags variable

--- modules/cloudtrail-baseline/variables.tf
+++ modules/cloudtrail-baseline/variables.tf
@@ -77,6 +77,7 @@ variable "s3_object_level_logging_buckets" {
 
 variable "tags" {
   description = "Specifies object tags key and value. This applies to all resources created by this module."
+  type        = map(string)
   default = {
     "Terraform" = true
   }
