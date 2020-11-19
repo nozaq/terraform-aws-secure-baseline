162d6e434134d1c7ca6485c280b4080cdc5b241d
Steph Gosling <steph@chuci.org>
2020-11-19 14:41:52 +0000

    fix: add types to other variables, fix typo in force_destroy description

--- modules/secure-bucket/variables.tf
+++ modules/secure-bucket/variables.tf
@@ -6,16 +6,19 @@ variable "log_bucket_name" {
 
 variable "lifecycle_glacier_transition_days" {
   description = "The number of days after object creation when the object is archived into Glacier."
+  type        = number
   default     = 90
 }
 
 variable "force_destroy" {
-  description = " A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
+  description = "A boolean that indicates all objects should be deleted from the bucket so that the bucket can be destroyed without error. These objects are not recoverable."
+  type        = bool
   default     = false
 }
 
 variable "enabled" {
   description = "A boolean that indicates this module is enabled. Resources are not created if it is set to false."
+  type        = bool
   default     = true
 }
 
