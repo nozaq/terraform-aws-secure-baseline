e676792be9397b1cb74840b2f34b8bc5020472b6
Juan Martin Corallo <jmcorallo@gmail.com>
2020-11-21 11:13:50 +0100

    Option to send VPC Logs to S3 instead of CW

--- modules/vpc-baseline/main.tf
+++ modules/vpc-baseline/main.tf
@@ -1,12 +1,28 @@
+locals {
+  is_cw_logs = var.vpc_log_destination_type == "cloud-watch-logs"
+}
+
 # --------------------------------------------------------------------------------------------------
-# Creates a log group for VPC Flow Logs
+# Enable VPC Flow Logs for the default VPC.
 # --------------------------------------------------------------------------------------------------
 
 resource "aws_cloudwatch_log_group" "default_vpc_flow_logs" {
-  count = var.enabled && var.enable_flow_logs ? 1 : 0
+  count = var.enabled && var.enable_flow_logs && local.is_cw_logs ? 1 : 0
 
   name              = var.vpc_log_group_name
   retention_in_days = var.vpc_log_retention_in_days
+
+  tags = var.tags
+}
+
+resource "aws_flow_log" "default_vpc_flow_logs" {
+  count = var.enabled && var.enable_flow_logs ? 1 : 0
+
+  log_destination_type = var.vpc_log_destination_type
+  log_destination      = local.is_cw_logs ? aws_cloudwatch_log_group.default_vpc_flow_logs[0].arn : var.vpc_flow_logs_s3_arn
+  iam_role_arn         = local.is_cw_logs ? var.vpc_flow_logs_iam_role_arn : null
+  vpc_id               = aws_default_vpc.default[0].id
+  traffic_type         = "ALL"
 
   tags = var.tags
 }
@@ -63,19 +79,3 @@ resource "aws_default_security_group" "default" {
     { Name = "Default Security Group" }
   )
 }
-
-# --------------------------------------------------------------------------------------------------
-# Enable VPC Flow Logs for the default VPC.
-# --------------------------------------------------------------------------------------------------
-
-resource "aws_flow_log" "default_vpc_flow_logs" {
-  count = var.enabled && var.enable_flow_logs ? 1 : 0
-
-  log_destination = aws_cloudwatch_log_group.default_vpc_flow_logs[0].arn
-  iam_role_arn    = var.vpc_flow_logs_iam_role_arn
-  vpc_id          = aws_default_vpc.default[0].id
-  traffic_type    = "ALL"
-
-  tags = var.tags
-}
-
--- modules/vpc-baseline/variables.tf
+++ modules/vpc-baseline/variables.tf
@@ -8,6 +8,11 @@ variable "enable_flow_logs" {
   default     = true
 }
 
+variable "vpc_log_destination_type" {
+  description = "The type of the logging destination. Valid values: cloud-watch-logs, s3"
+  default     = "cloud-watch-logs"
+}
+
 variable "vpc_log_group_name" {
   description = "The name of CloudWatch Logs group to which VPC Flow Logs are delivered."
 }
@@ -17,7 +22,11 @@ variable "vpc_flow_logs_iam_role_arn" {
 }
 
 variable "vpc_log_retention_in_days" {
-  description = "Number of days to retain logs for. CIS recommends 365 days.  Possible values are: 0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653. Set to 0 to keep logs indefinitely."
+  description = "Number of days to retain logs for. Only applies if vpc_log_destination_type is cloud-watch-logs. CIS recommends 365 days. Possible values are: 0, 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653. Set to 0 to keep logs indefinitely."
+}
+
+variable "vpc_flow_logs_s3_arn" {
+  description = "ARN of the S3 bucket where logs are sent. Only applies if vpc_log_destination_type is s3"
 }
 
 variable "tags" {
