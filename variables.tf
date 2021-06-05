5c037911c182b8f5d5549d0c5f31ceddebdbfe33
Dominic DePasquale <dad264@users.noreply.github.com>
2021-06-05 11:57:43 -0400

    fix: allow alarm variables to be set at top level module (#178)
    Co-authored-by: nozaq <code@nozaq.com>

--- variables.tf
+++ variables.tf
@@ -353,6 +353,80 @@ variable "alarm_sns_topic_name" {
   description = "The name of the SNS Topic which will be notified when any alarm is performed."
   default     = "CISAlarm"
 }
+variable "unauthorized_api_calls_enabled" {
+  description = "The boolean flag whether the unauthorized_api_calls alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "no_mfa_console_signin_enabled" {
+  description = "The boolean flag whether the no_mfa_console_signin alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "root_usage_enabled" {
+  description = "The boolean flag whether the root_usage alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "iam_changes_enabled" {
+  description = "The boolean flag whether the iam_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "cloudtrail_cfg_changes_enabled" {
+  description = "The boolean flag whether the cloudtrail_cfg_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "console_signin_failures_enabled" {
+  description = "The boolean flag whether the console_signin_failures alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "disable_or_delete_cmk_enabled" {
+  description = "The boolean flag whether the disable_or_delete_cmk alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "s3_bucket_policy_changes_enabled" {
+  description = "The boolean flag whether the s3_bucket_policy_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "aws_config_changes_enabled" {
+  description = "The boolean flag whether the aws_config_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "security_group_changes_enabled" {
+  description = "The boolean flag whether the security_group_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "nacl_changes_enabled" {
+  description = "The boolean flag whether the nacl_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "network_gw_changes_enabled" {
+  description = "The boolean flag whether the network_gw_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "route_table_changes_enabled" {
+  description = "The boolean flag whether the route_table_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "vpc_changes_enabled" {
+  description = "The boolean flag whether the vpc_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
+
+variable "organizations_changes_enabled" {
+  description = "The boolean flag whether the organizations_changes alarm is enabled or not. No resources are created when set to false."
+  default     = true
+}
 
 # --------------------------------------------------------------------------------------------------
 # Variables for guardduty-baseline module.
