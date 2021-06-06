30d197aa37c47619cc6d73fac6b6ea75e68e54db
nozaq <code@nozaq.com>
2021-06-06 14:56:31 +0900

    feat: enable S3 account-level public block (#188)
    This change tries to block any public access to all S3 buckets by
    default.
    fixes #176

---
+++ modules/s3-baseline/main.tf
@@ -0,0 +1,10 @@
+# --------------------------------------------------------------------------------------------------
+# S3 account-level Public Access Block
+# --------------------------------------------------------------------------------------------------
+resource "aws_s3_account_public_access_block" "this" {
+  block_public_acls       = var.block_public_acls
+  block_public_policy     = var.block_public_policy
+  ignore_public_acls      = var.ignore_public_acls
+  restrict_public_buckets = var.restrict_public_buckets
+}
+
---
+++ modules/s3-baseline/variables.tf
@@ -0,0 +1,19 @@
+variable "block_public_acls" {
+  description = "Whether Amazon S3 should block public ACLs for buckets in this account. Defaults to true."
+  default     = true
+}
+
+variable "block_public_policy" {
+  description = "Whether Amazon S3 should block public bucket policies for buckets in this account. Defaults to true."
+  default     = true
+}
+
+variable "ignore_public_acls" {
+  description = "Whether Amazon S3 should ignore public ACLs for buckets in this account. Defaults to true."
+  default     = true
+}
+
+variable "restrict_public_buckets" {
+  description = "Whether Amazon S3 should restrict public bucket policies for buckets in this account. Defaults to true."
+  default     = true
+}
