c2c735d422f44dad18e438a7cbc1d478b027a3ec
nozaq <code@nozaq.com>
2021-06-27 08:01:14 +0900

    refactor: define `configuration_aliases` (#196)
    Replace empty provider alias configurations with
    `configuration_aliases`.

--- README.md
+++ README.md
@@ -110,7 +110,7 @@ This module is composed of several submodules and each of which can be used inde
 
 | Name | Version |
 |------|---------|
-| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.13 |
+| <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) | >= 0.15 |
 | <a name="requirement_aws"></a> [aws](#requirement\_aws) | >= 3.39.0 |
 
 ## Providers
--- main.tf
+++ main.tf
@@ -1,3 +1,29 @@
+terraform {
+  required_version = ">= 0.15"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.39.0"
+
+      # A provider alias should be passed for each AWS region.
+      # Reference: https://docs.aws.amazon.com/general/latest/gr/rande.html
+      configuration_aliases = [
+        aws.ap-northeast-1, aws.ap-northeast-2, aws.ap-northeast-3,
+        aws.ap-south-1,
+        aws.ap-southeast-1, aws.ap-southeast-2,
+        aws.ca-central-1,
+        aws.eu-central-1,
+        aws.eu-north-1,
+        aws.eu-west-1, aws.eu-west-2, aws.eu-west-3,
+        aws.sa-east-1,
+        aws.us-east-1, aws.us-east-2,
+        aws.us-west-1, aws.us-west-2,
+      ]
+    }
+  }
+}
+
 locals {
   is_individual_account = var.account_type == "individual"
   is_master_account     = var.account_type == "master"
