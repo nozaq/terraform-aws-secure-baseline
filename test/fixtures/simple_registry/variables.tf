7203ffb128022ed8ec1cc36e1af436f0fd01b1d1
nozaq <code@nozaq.com>
2021-09-18 16:40:39 +0900

    chore: add integration tests

---
+++ test/fixtures/simple_registry/variables.tf
@@ -0,0 +1,8 @@
+variable "audit_s3_bucket_name" {
+  description = "The name of the S3 bucket to store various audit logs."
+}
+
+variable "region" {
+  description = "The AWS region in which global resources are set up."
+  default     = "us-east-1"
+}
---
+++ test/go.mod
@@ -0,0 +1,9 @@
+module github.com/nozaq/terraform-aws-secure-baseline
+
+go 1.16
+
+require (
+	github.com/google/uuid v1.3.0
+	github.com/gruntwork-io/terratest v0.37.8
+	github.com/stretchr/testify v1.7.0 // indirect
+)
