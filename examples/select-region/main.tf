dd1be65a3a36943e9bafc984373b93d962b1993f
nozaq <code@nozaq.com>
2021-09-18 10:52:35 +0900

    refactor: add version constraints consistent to the module itself

--- examples/select-region/main.tf
+++ examples/select-region/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 0.15"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.39.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
