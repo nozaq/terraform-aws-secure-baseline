5f8ca1c167ba94b79e48801bc68f385405830767
nozaq <code@nozaq.com>
2022-01-10 20:52:56 +0900

    fix: upgrade minimum provider requirements

--- examples/external-bucket/main.tf
+++ examples/external-bucket/main.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.15"
+  required_version = ">= 1.1.3"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.50.0"
+      version = ">= 3.66.0"
     }
   }
 }
--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.50.0"
+      version = ">= 3.66.0"
 
       # A provider alias should be passed for each AWS region.
       # Reference: https://docs.aws.amazon.com/general/latest/gr/rande.html
